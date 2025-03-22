3eba57cbeda4c89fa722ac55c9f0096f4ce3f45f
pattplatt <pattplatt@gmail.com>
2025-03-22 19:16:45 +0100

    updated contact icons to show information when hoovering over, added mail copy function

---
+++ src/components/ContactPage.vue
@@ -0,0 +1,38 @@
+<template>
+    <h2 class="text-2xl font-bold mb-4">Contact</h2>
+
+    <div class="flex space-x-10">
+        <div class="relative group cursor-pointer" @click="openLink(contactLinks.linkedin)">
+            <font-awesome-icon :icon="['fab', 'linkedin']" class="text-6xl icon-color" />
+            <div class="absolute left-1/2 transform -translate-x-1/2 mt-2 px-4 py-1 bg-gray-800 text-white text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                LinkedIn
+            </div>
+        </div>
+        <div class="relative group cursor-pointer" @click="openLink(contactLinks.github)">
+            <font-awesome-icon :icon="['fab', 'github']" class="text-6xl icon-color" />
+            <div class="absolute left-1/2 transform -translate-x-1/2 mt-2 px-4 py-1 bg-gray-800 text-white text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                GitHub
+            </div>
+        </div>
+        <div class="relative group cursor-pointer" @click="copyEmail">
+            <font-awesome-icon :icon="['fas', 'envelope']" class="text-6xl icon-color" />
+            <div class="absolute left-1/2 transform -translate-x-1/2 mt-2 px-4 py-1 bg-gray-800 text-white text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                {{ contactLinks.email.replace('mailto:', '') }}
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script setup>
+import { contactLinks } from '../data/personal_data.js';
+
+function openLink(url) {
+    window.open(url, '_blank');
+}
+
+function copyEmail() {
+    navigator.clipboard.writeText(contactLinks.email.replace('mailto:', ''));
+    alert('Email copied to clipboard!');
+}
+</script>
