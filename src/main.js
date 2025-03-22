7c3d7fffda0b55e6acd8909efa308e127ff1d6a1
pattplatt <pattplatt@gmail.com>
2025-03-22 18:29:57 +0100

    added icon import

--- src/main.js
+++ src/main.js
@@ -2,10 +2,18 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router.js';
 
-import './assets/main.css'; // If you have global styles
+import { library } from '@fortawesome/fontawesome-svg-core'
 
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
+import './assets/main.css';
+library.add(faLinkedin, faGithub, faEnvelope)
 const app = createApp(App);
-
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(router);
 
 app.mount('#app');
