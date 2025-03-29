930accb0988d88e0ea86958cd3cde189558f8bcd
pattplatt <pattplatt@gmail.com>
2025-03-29 19:41:25 +0100

    added introducing text, made css adjusments

--- src/components/HomeIntro.vue
+++ src/components/HomeIntro.vue
@@ -1,10 +1,13 @@
 <template>
-  <section class="home-intro">
-    <div class="home-intro flex flex-col items-center text-center mt-16">
-      <img :src="homeIntro.imageSrc" class="w-[300px] block mx-auto transition-transform hover:scale-105 filter grayscale hover:grayscale-0 transition duration-500" />
-      <h1 class="!text-[var(--accent-color)]">{{ homeIntro.greeting }}</h1>
-      <p>{{ homeIntro.description }}</p>
-    </div>
+  <section class="home-intro flex flex-col text-left mt-16 w-full max-w-6xl mx-auto px-4">
+    <img :src="homeIntro.imageSrc"
+      class="w-[300px] block mx-auto transition-transform hover:scale-105 filter grayscale hover:grayscale-0 transition duration-500" />
+    <h1>{{ homeIntro.greeting }}</h1>
+    <p class="text-base mt-4">
+      {{ homeIntro.description }}
+    </p>
+    <p class="text-sm mt-2 text-gray-600" v-html="homeIntro.detailedDescription">
+    </p>
   </section>
 </template>
 
