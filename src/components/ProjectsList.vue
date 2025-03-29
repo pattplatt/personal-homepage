c06ed0f6045ccf8f00fdd390ad265cec7138079c
pattplatt <pattplatt@gmail.com>
2025-03-29 19:31:43 +0100

    replaced article tag with lists, replaced description with short description to take up less space

--- src/components/ProjectsList.vue
+++ src/components/ProjectsList.vue
@@ -1,16 +1,19 @@
 <template>
   <section class="projects">
-    <h2 class="home-intro flex flex-col items-center text-center mt-16"> My theses, dissertations and projects</h2>
-    <div class="projects-container">
-      <article v-for="project in projects" :key="project.id" class="project-item">
+    <h2 class="home-intro flex flex-col items-center text-center mt-10"> My theses, dissertations and projects</h2>
+    <ul class="projects-container">
+      <li v-for="project in projects" :key="project.id" class="project-item">
         <h3>{{ project.title }}</h3>
-        <p>{{ project.description }}</p>
-        <router-link :to="`/projects/${project.id}`" class="underline link-accent">          View Project
+        <p>{{ project.short_description }}</p>
+        <router-link :to="`/projects/${project.id}`" class="underline link-accent">
+          View Project
         </router-link>
-      </article>
-    </div>
+      </li>
+    </ul>
+
   </section>
 </template>
+
 
 <script setup>
 import { projects } from '../data/projects.js';
